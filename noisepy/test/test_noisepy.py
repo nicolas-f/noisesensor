f020fc8048fd9aa9e5e3e5d3c20cb0cd76b91b5d
nicolas-f <[omitted]>
2018-04-06 16:58:40 +0200

    assert ok

--- noisepy/test/test_noisepy.py
+++ noisepy/test/test_noisepy.py
@@ -11,6 +11,8 @@ logging.basicConfig(level=logging.DEBUG)
 class TestAcousticIndicators(unittest.TestCase):
 
     def test_leq(self):
+        expected_leqs = [-26.21,-27.94,-29.12,-28.92,-40.39,-24.93,-31.55,-29.04,-31.08,-30.65]
+        results = []
         np = noisepy.noisepy(False, 32767.)
         f = open("../../core/test/speak_32000Hz_16bitsPCM_10s.raw", "rw")
         while not f is None:
@@ -19,9 +21,10 @@ class TestAcousticIndicators(unittest.TestCase):
                 break
             res = np.push(data, len(data) / 2)
             if not res is None:
-                print len(data) / 2
-                print str(res)+" dB"
+                results.append(res)
         f.close()
+        for i in range(len(expected_leqs)):
+            self.assertAlmostEqual(expected_leqs[i], results[i], 1)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestAcousticIndicators)
