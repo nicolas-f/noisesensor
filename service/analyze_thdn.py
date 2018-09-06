3daa3abc715e435e4cd269ca8a1fcb9bab1bf433
nicolas-f <[omitted]>
2018-09-06 13:45:12 +0200

    matlab code give weird results

--- service/analyze_thdn.py
+++ service/analyze_thdn.py
@@ -1,13 +1,61 @@
 import scipy.io.wavfile
 import scipy.signal
+from scipy.interpolate import interp1d
 import numpy as np
 import sys
 import math
-from matplotlib import pyplot
 
 
 def rms(signal):
     return math.sqrt(np.mean(signal**2))
+
+# Converted from https://fr.mathworks.com/matlabcentral/fileexchange/40455-computes-the-total-harmonic-distortion-thd-of-a-signal
+def THDNoctave(f0, signal, sample_rate):
+    signal = signal.astype(np.double)
+    sample_rate = float(sample_rate)
+    if not rms(signal) > 0:
+        print "no signal"
+        return
+    t = np.arange(0, len(signal)/sample_rate, 1/sample_rate)
+    T = len(signal)/sample_rate
+    freq = f0
+    # condition input time vector
+    input_error = 0
+    # add two samples to complete the last cycle.
+    dtt = 1/sample_rate
+    x = signal
+
+    # truncate extra samples, to fit in an integer number of cycles of freq
+    T = math.floor(T * freq) / freq
+
+    # resample on a linear grid:
+    # t1, x1 is the new input, not including the last sample
+    x = x - sum(x)/len(x)  # remove any DC offset
+
+    N = max(1e6, len(x))   # number of samples
+    dt = T/N
+    t1 = np.arange(0, t[-1], dt)  # 0:dt:(T-dt);
+    x1 = interp1d(t, x, kind='cubic')(t1) #interp1(t,x,t1,'cubic');
+
+    # compute cos-sin fourier coefficients
+    w = 2*math.pi*freq
+    acs = (2/T) * sum(x1*np.cos(w*t1))*dt  # basic frequency cos coefficient.
+    bsn = (2/T) * sum(x1*np.sin(w*t1))*dt  # basic frequency sin coefficient.
+    amp = (acs**2 + bsn**2)**0.5
+    ph = math.pi/2 - np.sign(acs) * math.acos(bsn/amp)
+
+    rms22 = (2/T) * sum(x1**2) * dt
+    THD = (rms22/amp**2 - 1)**0.5
+
+    # correct phase to be in the range [-pi : pi]
+    if ph > math.pi:
+        ph = ph - 2*math.pi
+    if ph < -math.pi:
+        ph = ph + 2*math.pi
+
+    print "THD+N:     %.1f%% or %.1f dB" % (THD * 100, 20 * math.log10(THD))
+
+    return THD, ph, amp
 
 
 def THDN(f0, signal, sample_rate):
@@ -16,9 +64,7 @@ def THDN(f0, signal, sample_rate):
     if not rms(signal) > 0:
         print "no signal"
         return
-    # remove first 2000 samples
-    if len(signal) < sample_rate:
-        sys.exit("Not enough signal length")
+
 
     #f0 Frequency to be removed from signal (Hz)
     q = math.trunc(math.sqrt(f0) / 2.)
@@ -37,6 +83,8 @@ def THDN(f0, signal, sample_rate):
     # thdn is (noise+harmonic) / fundamental
     thdn = other_rms / total_rms
     print "THD+N:     %.1f%% or %.1f dB" % (thdn * 100, 20 * math.log10(thdn))
+
+    return (thdn, 0, 0)
 
 def analyze_channels(freq, filename, function):
     """
@@ -64,7 +112,16 @@ def analyze_channels(freq, filename, function):
         for ch_no, channel in enumerate(signal.transpose()):
             print '-- Channel %d --' % (ch_no + 1)
             function(freq, channel, sample_rate)
-
+def testSquare():
+    # Square signal analytic expected result
+    analytic_result = math.sqrt((math.pi**2/8.)-1)
+    freq = 10.
+    number_of_cycles = 4
+    dt = 0.0001
+    t = np.arange(0, number_of_cycles / freq, dt)
+    x = (np.mod(t, 1. / freq) < 0.5 / freq)
+    x = 1 * (2 * x - 1)
+    print "expected %.4f got %.4f " % (analytic_result, THDN(freq, x, 1/dt)[0])
 
 if len(sys.argv) >= 2:
     files = sys.argv[2:]
@@ -77,5 +134,6 @@ if len(sys.argv) >= 2:
             print e
         print ''
 else:
+    testSquare()
     sys.exit("You must provide the test frequency and at least one file to analyze")
 
