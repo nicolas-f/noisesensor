8c4f905a3baaca14b93ae64fd1bd92c371a971e6
nicolas-f <[omitted]>
2019-08-21 11:24:53 +0200

    color bars with trigger value

--- service/audio_trigger_test.py
+++ service/audio_trigger_test.py
@@ -2,6 +2,12 @@ import numpy
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
+
+def autocolor(bar):
+    for col in bar:
+        if col.get_height() > 0.995:
+            col.set_color('r')
+
 trigger = [40.49, 39.14, 34.47, 30.5, 39.54, 31.98, 38.37, 43.84, 36.09, 43.72, 40.55, 39.25, 39.15, 38.36, 38.3, 36.58,
            39.9, 47.76, 51.64, 37.2, 44.89, 46.6, 51.08, 37.77, 28, 29.59, 30.25, 23.16, 25.74]
 weight = [0.04,0.04,0.04,0.04,0.04,0.04,0.04,0.14,0.14,0.14,0.14,0.14,0.14,0.14,0.14,0.14,0.14,0.14,0.14, 0.24, 0.41,
@@ -13,9 +19,12 @@ test1_spectrum = numpy.genfromtxt('test/test1_near.csv', delimiter=',', skip_hea
 
 test2_spectrum = numpy.genfromtxt('test/test2_far_far.csv', delimiter=',', skip_header=1, usecols=range(5, 34))
 
+test3_spectrum = numpy.genfromtxt('test/test_background.csv', delimiter=',', skip_header=1, usecols=range(5, 34))
+
 dist0 = numpy.ones(len(ref_spectrum)) - [distance.cosine(trigger, ref_spectrum[idfreq], w=weight) for idfreq in range(len(ref_spectrum))]
 dist1 = numpy.ones(len(ref_spectrum)) - [distance.cosine(trigger, test1_spectrum[idfreq], w=weight) for idfreq in range(len(ref_spectrum))]
 dist2 = numpy.ones(len(ref_spectrum)) - [distance.cosine(trigger, test2_spectrum[idfreq], w=weight) for idfreq in range(len(ref_spectrum))]
+dist3 = numpy.ones(len(ref_spectrum)) - [distance.cosine(trigger, test3_spectrum[idfreq], w=weight) for idfreq in range(len(ref_spectrum))]
 
 ref_spectrum = numpy.rot90(ref_spectrum)
 
@@ -23,29 +32,42 @@ test1_spectrum = numpy.rot90(test1_spectrum)
 
 test2_spectrum = numpy.rot90(test2_spectrum)
 
-plt.subplot(3, 2, 1)
+test3_spectrum = numpy.rot90(test3_spectrum)
+
+plt.subplot(4, 2, 1)
 
 plt.imshow(ref_spectrum)
 
-ax1 = plt.subplot(3, 2, 2)
+ax1 = plt.subplot(4, 2, 2)
 
-ax1.set_ylim(0.98, 1.0)
+ax1.set_ylim(0.96, 1.0)
 
-plt.bar(numpy.arange(len(dist0)), dist0)
+autocolor(plt.bar(numpy.arange(len(dist0)), dist0))
 
-plt.subplot(3, 2, 3)
+plt.subplot(4, 2, 3)
 
 plt.imshow(test1_spectrum)
 
-plt.subplot(3, 2, 4, sharey=ax1)
+plt.subplot(4, 2, 4, sharey=ax1)
 
-plt.bar(numpy.arange(len(dist1)), dist1)
+autocolor(plt.bar(numpy.arange(len(dist1)), dist1))
 
-plt.subplot(3, 2, 5)
+plt.subplot(4, 2, 5)
 
 plt.imshow(test2_spectrum)
 
-plt.subplot(3, 2, 6, sharey=ax1)
-plt.bar(numpy.arange(len(dist2)), dist2)
+plt.subplot(4, 2, 6, sharey=ax1)
+autocolor(plt.bar(numpy.arange(len(dist2)), dist2))
+
+
+plt.subplot(4, 2, 7)
+
+plt.imshow(test3_spectrum)
+
+plt.subplot(4, 2, 8, sharey=ax1)
+plt.bar(numpy.arange(len(dist2)), dist3)
+
+
+
 
 plt.show()
