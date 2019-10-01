55d1db3b84037371e36f9a9f9de25073e9a6c8ab
nicolas-f <[omitted]>
2019-10-01 11:17:15 +0200

    encode and encrypt samples in noisesensor

--- service/noisesensor.py
+++ service/noisesensor.py
@@ -36,11 +36,21 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from __future__ import print_function
 
 try:
+    # For Python 3.0 and later
     from http.server import HTTPServer
     from http.server import BaseHTTPRequestHandler
     from http import HTTPStatus
 except ImportError:
+    # Fall back to Python 2's urllib2
     from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+    from urllib.error import URLError
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen, URLError
 
 import noisepy
 import getopt
@@ -49,6 +59,25 @@ import threading
 import datetime
 import collections
 import time
+import json
+import ssl
+import math
+import numpy
+import io
+
+try:
+    from Crypto.Cipher import PKCS1_OAEP
+    from Crypto.PublicKey import RSA
+    from Crypto.Cipher import AES
+    from Crypto import Random
+    from Crypto.Random import get_random_bytes
+except ImportError:
+    print("Please install PyCrypto")
+    print("pip install pycrypto")
+
+import soundfile as sf
+
+
 
 ## Usage ex
 # arecord -D hw:1 -r 48000 -f S32_LE -c2 -t raw | python -u noisesensor.py -c 2 -f S32_LE -r 48000 -p 8090
@@ -72,6 +101,10 @@ class AcousticIndicatorsProcessor(threading.Thread):
         for fun in self.data["callback_slow"]:
             fun(line)
 
+    def push_samples(self, samples):
+        for fun in self.data["callback_samples"]:
+            fun(samples)
+
     def unix_time(self):
         return (datetime.datetime.utcnow() - self.epoch).total_seconds()
 
@@ -85,48 +118,209 @@ class AcousticIndicatorsProcessor(threading.Thread):
         start = 0
         total_bytes_read = 0
         bytes_per_seconds = [32000.0, 48000.0][self.data["rate"]] * [2, 4][["S16_LE", "S32_LE"].index(self.data["sample_format"])] * (1 if self.data["mono"] else 2)
-        while True:
-            audiosamples = sys.stdin.read(np.max_samples_length())
-            total_bytes_read += len(audiosamples)
-            if self.data["debug"]:
-                # Pause stream before gethering more samples
-                cur = time.time()
-                samples_time = len(audiosamples) / bytes_per_seconds
-                if cur - start < samples_time:
-                    time.sleep(samples_time - (cur - start))
-                start = time.time()
-            if not audiosamples:
-                print("%s End of audio samples, duration %.3f secondes" % (datetime.datetime.now().isoformat(), total_bytes_read / bytes_per_seconds))
-                break
-            else:
-                resp = np.push(audiosamples, len(audiosamples))
-                leq125ms = 0
-                laeq125ms = 0
-                leq1s = 0
-                laeq1s = 0
-                leqSpectrum = []
-                push_fast = False
-                push_slow = False
-                # time can be in iso format using datetime.datetime.now().isoformat()
-                if resp == noisepy.feed_complete or resp == noisepy.feed_fast:
-                    leq125ms = np.get_leq_fast()
-                    leqSpectrum = map(np.get_leq_band_fast, range(len(freqs)))
-                    push_fast = True
-                    if resp == noisepy.feed_complete:
-                        leq1s = np.get_leq_slow()
-                        push_slow = True
-                resp = npa.push(audiosamples, len(audiosamples))
-                if resp == noisepy.feed_complete or resp == noisepy.feed_fast:
-                    laeq125ms = npa.get_leq_fast()
-                    push_fast = True
-                    if resp == noisepy.feed_complete:
-                        laeq1s = npa.get_leq_slow()
-                        push_slow = True
-                if push_fast:
-                    self.push_data_fast([self.unix_time(), leq125ms, laeq125ms] + leqSpectrum)
-                if push_slow:
-                    self.push_data_slow([self.unix_time(), leq1s, laeq1s])
+        try:
+            while True:
+                audiosamples = sys.stdin.read(np.max_samples_length())
+                if not audiosamples:
+                    print("%s End of audio samples, duration %.3f secondes" % (datetime.datetime.now().isoformat(), total_bytes_read / bytes_per_seconds))
+                    break
+                else:
+                    total_bytes_read += len(audiosamples)
+                    self.push_samples(audiosamples)
+                    if self.data["debug"]:
+                        # Pause stream before gathering more samples
+                        cur = time.time()
+                        samples_time = len(audiosamples) / bytes_per_seconds
+                        if cur - start < samples_time:
+                            time.sleep(samples_time - (cur - start))
+                        start = time.time()
+                    resp = np.push(audiosamples, len(audiosamples))
+                    leq125ms = 0
+                    laeq125ms = 0
+                    leq1s = 0
+                    laeq1s = 0
+                    leqSpectrum = []
+                    push_fast = False
+                    push_slow = False
+                    # time can be in iso format using datetime.datetime.now().isoformat()
+                    if resp == noisepy.feed_complete or resp == noisepy.feed_fast:
+                        leq125ms = np.get_leq_fast()
+                        leqSpectrum = map(np.get_leq_band_fast, range(len(freqs)))
+                        push_fast = True
+                        if resp == noisepy.feed_complete:
+                            leq1s = np.get_leq_slow()
+                            push_slow = True
+                    resp = npa.push(audiosamples, len(audiosamples))
+                    if resp == noisepy.feed_complete or resp == noisepy.feed_fast:
+                        laeq125ms = npa.get_leq_fast()
+                        push_fast = True
+                        if resp == noisepy.feed_complete:
+                            laeq1s = npa.get_leq_slow()
+                            push_slow = True
+                    if push_fast:
+                        self.push_data_fast([self.unix_time(), leq125ms, laeq125ms] + leqSpectrum)
+                    if push_slow:
+                        self.push_data_slow([self.unix_time(), leq1s, laeq1s])
+        finally:
+            self.data["running"] = False
 
+
+class TriggerProcessor(threading.Thread):
+    def __init__(self, data):
+        threading.Thread.__init__(self)
+        self.data = data
+        self.config = {}
+        self.fast = collections.deque(maxlen=data['row_cache_fast'])
+        self.sample_rate = [32000.0, 48000.0][self.data["rate"]]
+        self.bytes_per_seconds = self.sample_rate * [2, 4][["S16_LE", "S32_LE"].index(self.data["sample_format"])] * (1 if self.data["mono"] else 2)
+        self.samples_stack = None
+        self.samples_trigger = []
+        self.remaining_samples = 0
+        self.last_fetch_trigger_info = 0
+        self.epoch = datetime.datetime.utcfromtimestamp(0)
+
+    def push_data_samples(self, samples):
+        stack = self.samples_stack
+        if stack is not None:
+            stack.append(samples)
+
+    def push_data_fast(self, line):
+        self.fast.append(line)
+
+    # from scipy
+    def _validate_weights(self,w, dtype=numpy.double):
+        w = self._validate_vector(w, dtype=dtype)
+        if numpy.any(w < 0):
+            raise ValueError("Input weights should be all non-negative")
+        return w
+
+    # from scipy
+    def _validate_vector(self,u, dtype=None):
+        # XXX Is order='c' really necessary?
+        u = numpy.asarray(u, dtype=dtype, order='c').squeeze()
+        # Ensure values such as u=1 and u=[1] still return 1-D arrays.
+        u = numpy.atleast_1d(u)
+        if u.ndim > 1:
+            raise ValueError("Input vector should be 1-D.")
+        return u
+
+    # from scipy
+    def dist_cosine(self,u, v, w=None):
+        u = self._validate_vector(u)
+        v = self._validate_vector(v)
+        if w is not None:
+            w = self._validate_weights(w)
+        uv = numpy.average(u * v, weights=w)
+        uu = numpy.average(numpy.square(u), weights=w)
+        vv = numpy.average(numpy.square(v), weights=w)
+        dist = 1.0 - uv / numpy.sqrt(uu * vv)
+        return dist
+
+    def check_hour(self):
+        t = datetime.datetime.now()
+        if "start_hour" not in self.config:
+            return True
+        h, m = map(int, self.config["start_hour"].split(":"))
+        start_ok = False
+        if t.hour > h or (t.hour == h and t.minute >= m):
+            start_ok = True
+        end_ok = False
+        if "end_hour" not in self.config:
+            end_ok = True
+        else:
+            h, m = map(int, self.config["end_hour"].split(":"))
+            if t.hour < h or (t.hour == h and t.minute < m):
+                end_ok = True
+        return start_ok and end_ok
+
+    def encrypt(self, audio_data):
+        output_encrypted = io.BytesIO()
+
+        # Open public key to encrypt AES key
+        key = RSA.importKey(self.config["file"])
+        cipher = PKCS1_OAEP.new(key)
+
+        aes_key = get_random_bytes(AES.block_size)
+        iv = Random.new().read(AES.block_size)
+
+        # Write OAEP header
+        output_encrypted.write(cipher.encrypt(aes_key + iv))
+
+        aes_cipher = AES.new(aes_key, AES.MODE_CFB, iv)
+        # Write AES data
+        output_encrypted.write(aes_cipher.encrypt(audio_data))
+        return output_encrypted.getvalue()
+
+    def run(self):
+        status = "wait_trigger"
+        start_processing = self.unix_time()
+        while self.data["running"]:
+            if (self.data["debug"] or datetime.datetime.now().hour >= 23)\
+                    and time.time() - self.last_fetch_trigger_info >= 2 * 3600.0:
+                # Fetch trigger information
+                try:
+                    res = urlopen("https://127.0.0.1:4430/get-trigger", context=ssl._create_unverified_context())
+                    self.config = json.loads(res.read())
+                    if time.time() * 1000 < self.config["date_end"]:
+                        self.data["callback_fast"].append(self.push_data_fast)
+                        self.samples_stack = collections.deque(maxlen=math.ceil((self.bytes_per_seconds * 0.125) / (self.config["cached_length"] + 1)))
+                        self.data["callback_samples"].append(self.push_data_samples)
+                    self.last_fetch_trigger_info = time.time()
+                except [URLError, ValueError] as e:
+                    # ignore
+                    time.sleep(60)
+            elif self.config is not None and status == "wait_trigger" and self.config["trigger_count"] > 0:
+                cur_time = time.time() * 1000
+                if cur_time > self.config["date_end"]:
+                    self.samples_stack = None
+                elif cur_time > self.config["date_start"] and self.check_hour():
+                    # Time condition ok
+                    # now check audio condition
+                    while len(self.fast) > 0:
+                        try:
+                            spectrum = self.fast.popleft()
+                            #print("%.3f %s" % (
+                            #spectrum[0] - start_processing, "\t".join(["%.2f" % v for v in spectrum[3:]])))
+                            laeq = spectrum[2]
+                            print(laeq)
+                            if laeq >= self.config["min_leq"]:
+                                # leq condition ok
+                                # check for spectrum condition
+                                cosine_similarity = 1 - self.dist_cosine(spectrum[3:], self.config["spectrum"], w=self.config["weight"])
+                                if int(spectrum[0] - start_processing) == 15:
+                                    cosine_similarity = 1
+                                else:
+                                    cosine_similarity = 0
+                                if cosine_similarity > self.config["cosine"] / 100.0:
+                                    status = "record"
+                                    self.remaining_samples = int(self.bytes_per_seconds * self.config["total_length"])
+                                    print("Start %.3f recording cosine:%.3f" % (spectrum[0] - start_processing, cosine_similarity))
+                                    self.config["trigger_count"] -= 1
+
+                        except IndexError:
+                            pass
+            elif status == "record":
+                while len(self.samples_stack) > 0:
+                    samples = self.samples_stack.popleft()
+                    size = min(self.remaining_samples, len(samples))
+                    self.samples_trigger.append(samples[:size])
+                    self.remaining_samples -= size
+                    if self.remaining_samples == 0:
+                        status = "wait_trigger"
+                        # Compress audio samples
+                        output = io.BytesIO()
+                        input = io.BytesIO(b"".join(self.samples_trigger))
+                        data, samplerate = sf.read(input, format='RAW', channels=1 if self.data['mono'] else 2, samplerate=int(self.sample_rate),
+                                                   subtype=['PCM_16', 'PCM_32'][['S16_LE', 'S32_LE'].index(self.data["sample_format"])])
+                        sf.write(output, data, samplerate, format='OGG')
+                        self.samples_trigger = []
+                        audio_data = output.getvalue()
+                        audio_data_encrypt = self.encrypt(audio_data)
+
+            time.sleep(0.125)
+
+    def unix_time(self):
+        return (datetime.datetime.utcnow() - self.epoch).total_seconds()
 
 class AcousticIndicatorsServer(HTTPServer):
     def __init__(self, data, *args, **kwargs):
@@ -202,12 +396,12 @@ def usage():
 # }
 def main():
     # Shared data between process
-    data = {'debug':False, 'leq': [], "callback_fast": [], "callback_slow": [], "row_cache_fast": 60 * 8, "row_cache_slow": 60,
+    data = {'running':True, 'debug':False, 'leq': [], "callback_fast": [], "callback_slow": [], "callback_samples": [], "row_cache_fast": 60 * 8, "row_cache_slow": 60,
             "format_fast" : b'%.3f,%.2f,%.2f,' + ",".join(["%.2f"]*len(freqs))+b'\n', "format_slow": b'%d,%.2f,%.2f\n' }
     # parse command line options
     port = 0
     try:
-        for opt, value in getopt.getopt(sys.argv[1:], "p:f:r:c:d")[0]:
+        for opt, value in getopt.getopt(sys.argv[1:], "p:f:r:c:d:")[0]:
             if opt == "-p":
                 port = int(value)
             elif opt == "-r":
@@ -219,12 +413,21 @@ def main():
                 data["mono"] = value == "1"
             elif opt == "-d":
                 data["debug"] = True
+                data["debug_file"] = value
     except getopt.error as msg:
         usage()
         exit(-1)
+
+    if data["debug"]:
+        sys.stdin = open(data["debug_file"], 'rb')
+
     # run audio processing thread
     processing_thread = AcousticIndicatorsProcessor(data)
     processing_thread.start()
+
+    # run trigger processing thread
+    trigger_thread = TriggerProcessor(data)
+    trigger_thread.start()
 
     # Http server
     if port > 0:
@@ -232,8 +435,10 @@ def main():
         httpserver.start()
 
     # End program when audio processing end
-    processing_thread.join()
-
+    try:
+        processing_thread.join()
+    finally:
+        data["running"] = False
 
 if __name__ == "__main__":
     main()
