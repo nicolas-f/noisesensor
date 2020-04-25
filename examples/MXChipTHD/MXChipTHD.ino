18b554bc5d0ecb671ac014d665d8a2c3baa0c2fe
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2020-04-25 13:48:53 +0200

    Display leq level

--- examples/MXChipTHD/MXChipTHD.ino
+++ examples/MXChipTHD/MXChipTHD.ino
@@ -32,6 +32,8 @@
 
 #define SAMPLE_SIZE 16
 
+#define SAMPLE_BYTE_SIZE 2
+
 // AUDIO_CHUNK_SIZE is 512 bytes
 // A sample is 2 bytes (16 bits)
 // raw_audio_buffer contains 128 short left samples and 128 short right samples
@@ -51,29 +53,27 @@ AudioClass& Audio = AudioClass::getInstance();
 // Audio buffer used by MXChip audio controler
 static char raw_audio_buffer[AUDIO_CHUNK_SIZE];
 
-// Audio buffer provided to QRTone. Samples are
-static float scaled_input_buffer[MAX_WINDOW_SIZE];
+static float last_leq_fast = 0.f;
 
+static float last_printed = 0.f;
 /**
  * Called by AudioClass when the audio buffer is full
  */
 void recordCallback(void)
 {
   int length = Audio.readFromRecordBuffer(raw_audio_buffer, AUDIO_CHUNK_SIZE);
-  // Convert Stereo short samples to mono float samples
-  char* cur_reader = &raw_audio_buffer[0];
-  char* end_reader = &raw_audio_buffer[length];
-  const int offset = 4; // short samples size + skip right channel
   int sample_index = 0;
-  while(cur_reader < end_reader)
+  int maxLen = ai_get_maximal_sample_size(acoustic_indicators);
+  while(sample_index < length)
   {
-    int16_t sample = *((int16_t *)cur_reader);
-    scaled_input_buffer[sample_index++] = (float)sample / 32768.0f;
-    cur_reader += offset;
-  }
-  // Push remaining samples
-  if(sample_index > 0) {
-    // push_samples(scaled_input_buffer, sample_index)
+    int window_length = min(length - sample_index, maxLen);
+    int res = ai_add_sample(acoustic_indicators, window_length, (int8_t*)(raw_audio_buffer + sample_index));
+    if (res == AI_FEED_FAST || res == AI_FEED_COMPLETE) {
+        // If 125ms or 1s analysis is complete
+        last_leq_fast = acoustic_indicators->last_leq_fast;
+        maxLen = ai_get_maximal_sample_size(acoustic_indicators);
+    }
+    sample_index += window_length;
   }
 }
 
@@ -86,12 +86,13 @@ void setup(void)
   int a_filter = 0;         // Usage of A weighting filter
   int compute_spectrum = 1; // Run FFT computation to have spectrum bands
   int use_tukey_window = 1; // Apply a tukey window in order to reduce frequency analysis leaks
+  int mono = 0;  // is audio stream is mono or stereo
 
   ai_init_acoustic_indicators_data(acoustic_indicators, a_filter, compute_spectrum, REF_SOUND_PRESSURE, use_tukey_window, AI_SAMPLE_RATE_32000, "S16_LE", 0);
   ai_SetTukeyAlpha(acoustic_indicators, 1.0); // Use Hann window instead of tukey
   Audio.format(SAMPLE_RATE, SAMPLE_SIZE);
   // disable automatic level control
-  // Audio.setPGAGain(0x3F);
+  Audio.setPGAGain(0x3F);
   
   // Start to record audio data
   Audio.startRecord(recordCallback);
@@ -101,11 +102,16 @@ void setup(void)
 
 void loop(void)
 {
-  delay(100);
+  if(last_leq_fast != last_printed) {
+    last_printed = last_leq_fast;
+    char buf[100];
+    sprintf(buf, "%.2f dB", last_leq_fast);
+    Screen.print(0, buf);
+  }
 }
 
 void printIdleMessage()
 {
   Screen.clean();
-  Screen.print(0, "Awaiting message");
+  Screen.print(0, "Awaiting audio");
 }
