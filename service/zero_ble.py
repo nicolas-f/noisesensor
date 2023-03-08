e605341793160e4f29f9ed48ab3b26e68590090d
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-03-08 16:24:20 +0100

    disconnect from ble device in order to reduce power consumtion

--- service/zero_ble.py
+++ service/zero_ble.py
@@ -15,38 +15,38 @@ def uart_data_received(sender, data):
 
 
 async def main(config):
-    address = None
-    while address is None:
-        logger.info("Discover BLE devices..")
-        devices = await BleakScanner.discover()
-        for d in devices:
-            if "Pixl.js" in d.name:
-                address = d.address
-                logger.info("Found Pixl.js " + repr(d))
-                break
-        if address is None:
-            logger.info("Could not find Pixl.js device.")
-    async with BleakClient(address) as client:
-        await client.start_notify(UUID_NORDIC_RX, uart_data_received)
-        context = zmq.Context()
-        socket = context.socket(zmq.SUB)
-        socket.connect(config.input_address)
-        socket.subscribe("")
-        while True:
-            # Retrieve JSON data from zero_trigger
-            logger.info("Waiting for next zmq message")
-            data = socket.recv_json()
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(config.input_address)
+    socket.subscribe("")
+    while True:
+        # Retrieve JSON data from zero_trigger
+        logger.info("Waiting for next zmq message")
+        data = socket.recv_json()
 
-            leq = data["leq"]
-            scores = data["scores"]
-            messages = ["leq: %.2f dB" % leq]
-            max_line = 7
-            for y, key_value in zip(range(len(scores)), sorted(scores.items(), key=lambda item: -item[1])):
-                tag = key_value[0][slice(None, 12)]
-                messages.append('{:12s}: {:.3f}'.format(tag, key_value[1]))
-                if y >= max_line:
+        leq = data["leq"]
+        scores = data["scores"]
+        messages = ["leq: %.2f dB" % leq]
+        max_line = 7
+        for y, key_value in zip(range(len(scores)), sorted(scores.items(), key=lambda item: -item[1])):
+            tag = key_value[0][slice(None, 12)]
+            messages.append('{:12s}: {:.3f}'.format(tag, key_value[1]))
+            if y >= max_line:
+                break
+        c = b"\x03\x10messages=%s;\n\x10updateScreen();\n" % repr(messages).encode('UTF-8')
+        address = None
+        while address is None:
+            logger.info("Discover BLE devices..")
+            devices = await BleakScanner.discover()
+            for d in devices:
+                if "Pixl.js" in d.name:
+                    address = d.address
+                    logger.info("Found Pixl.js " + repr(d))
                     break
-            c = b"\x03\x10messages=%s;\n\x10updateScreen();\n" % repr(messages).encode('UTF-8')
+            if address is None:
+                logger.info("Could not find Pixl.js device.")
+        async with BleakClient(address) as client:
+            await client.start_notify(UUID_NORDIC_RX, uart_data_received)
             while len(c) > 0:
                 await client.write_gatt_char(UUID_NORDIC_TX, bytearray(c[0:20]), True)
                 c = c[20:]
