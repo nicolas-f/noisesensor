fdc77f403ee3c6da1b7c8df67e2ef4671f6b1b72
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-07-28 13:52:42 +0200

    fix compose

--- services/ansible_openvpn/playbooks/files/openvpn_ansible.py
+++ services/ansible_openvpn/playbooks/files/openvpn_ansible.py
@@ -124,6 +124,7 @@ def main():
             log_text = file.read()
     else:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
         s.connect(("host.docker.internal", int(os.environ["MANAGEMENT_OPENVPN_PORT"])))
         sout = receive(s)
         if sout == 'ENTER PASSWORD:':
