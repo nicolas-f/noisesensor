f27c7c0ed97820a489a5856feefc040583a6958a
nicolas-f <github@nettrader.fr>
2024-01-15 16:11:39 +0100

    Add an option to write to binary file directly as windows is messing up pipes

--- services/zero_play.py
+++ services/zero_play.py
@@ -3,7 +3,6 @@
 import sys
 import zmq
 import argparse
-import struct
 
 def main():
     parser = argparse.ArgumentParser(description='This program read audio stream from zeromq and push to stdout',
@@ -12,6 +11,8 @@ def main():
 
     parser.add_argument("-p", "--port", help="Port to publish samples", default=10001, type=int)
     parser.add_argument("-i", "--interface", help="Interface to publish", default="127.0.0.1", type=str)
+    parser.add_argument("-o", "--output_file",
+                        help="Instead of output audio into stdout, write into the specified file", default="", type=str)
 
     args = parser.parse_args()
 
@@ -23,9 +24,16 @@ def main():
 
     out_buffer = sys.stdout.buffer
 
-    while True:
-        time_bytes, audio_data_bytes = socket.recv_multipart()
-        out_buffer.write(audio_data_bytes)
+    if len(args.output_file) > 0:
+        out_buffer = open(args.output_file, "wb")
+
+    try:
+        while True:
+            time_bytes, audio_data_bytes = socket.recv_multipart()
+            out_buffer.write(audio_data_bytes)
+    finally:
+        if len(args.output_file) > 0:
+            out_buffer.close()
 
 
 if __name__ == "__main__":
